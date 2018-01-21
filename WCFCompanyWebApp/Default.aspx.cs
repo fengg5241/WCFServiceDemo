1a39509660057fc7c1b8a06b97b9c1ac95fdd02a
fengg5241 <fengg5241@users.noreply.github.com>
2018-01-21 11:09:00 +0800

    Add files via upload

---
+++ WCFCompanyWebApp/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WCFCompanyWebApp.SupplierServiceReference;
+
+namespace WCFCompanyWebApp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SupplierServiceClient client = new SupplierServiceClient();
+
+            Label1.Text = client.insertOrder(12);
+        }
+    }
+}
